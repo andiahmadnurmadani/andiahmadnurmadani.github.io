6323cc55169db789389c910c8d787b5aa81f6160
andiahmadnurmadani <andiahmadnurmadani15@gmail.com>
2022-09-01 20:03:01 +0700

    edit

--- Music/js/music-list.js
+++ Music/js/music-list.js
@@ -11,84 +11,31 @@
 
 let allMusic = [
   {
-    name: "The Hills X I Was Never There",
-    artist: "The Weeknd",
-    img: "music1",
-    src: "music1"
-  },
-  {
     name: "I wanna be yours",
     artist: "Arctic Monkey",
-    img: "music2",
-    src: "music2"
-  },
-  {
-    name: "Under The Influence X I Was Never There",
-    artist: "Unknow",
-    img: "music3",
-    src: "music3"
-  },
-  {
-    name: "playboi carti",
-    artist: "gypsy woman",
-    img: "music4",
-    src: "music4"
-  },
-  {
-    name: "Seafret",
-    artist: "Atlantis",
-    img: "music6",
-    src: "music6"
-  },
-  
-  {
-    name: "Cause I Need Beauty And The Beast",
-    artist: "Justin Beiber",
-    img: "music7",
-    src: "music7"
-  },
-  {
-    name: "SNAP",
-    artist: "Rosa Linn",
-    img: "music8",
-    src: "music8"
-  },
-    {
-    name: "Doin' Time",
-    artist: "Lana Del Rey",
-    img: "music9",
-    src: "music9"
-  },
-   {
-    name: "years & years",
-    artist: "breathe",
-    img: "music10",
-    src: "music10"
-  },
-  {
-    name: "Lights",
-    artist: "Ellie Goulding",
-    img: "music11",
-    src: "music11"
+    img: "music1",
+    src: "music1"
   },
    {
     name: "I Wanna Love You",
     artist: "Akon",
-    img: "music12",
-    src: "music12"
+    img: "music2",
+    src: "music2"
   },
-   {
-    name: "Summertime Sadness",
-    artist: "Lana Del Rey",
-    img: "music13",
-    src: "music13"
+  {
+    name: "Kiss It Better x I Was Never There",
+    artist: "Rihanna x The Weeknd",
+    img: "music3",
+    src: "music3"
   },
-   {
-    name: "sex drugs etc",
-    artist: "beach weather",
-    img: "music14",
-    src: "music14"
+  {
+    name: "Asmalibrasi",
+    artist: "Soegi Bornean",
+    img: "music4",
+    src: "music4"
   },
+
+
 
 
 
