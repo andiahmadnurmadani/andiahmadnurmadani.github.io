67c66223d139c4b035045154858c30f71a658107
unknown <kir17garutwebsite@gmai.com>
2023-01-08 14:51:18 +0700

    V1

--- Music/js/music-list.js
+++ Music/js/music-list.js
@@ -11,43 +11,17 @@
 
 let allMusic = [
   {
-    name: "I wanna be yours",
-    artist: "Arctic Monkey",
+    name: "All Girl Are The Same",
+    artist: "Juice WRLD",
     img: "music1",
     src: "music1"
   },
   {
-    name: "I Wanna Love You",
-    artist: "Akon",
+    name: "Riptide",
+    artist: "Vance Joy",
     img: "music2",
     src: "music2"
   },
-  {
-    name: "Kiss It Better x I Was Never There",
-    artist: "Rihanna x The Weeknd",
-    img: "music3",
-    src: "music3"
-  },
-  {
-    name: "Asmalibrasi",
-    artist: "Soegi Bornean",
-    img: "music4",
-    src: "music4"
-  },
-  {
-    name: "Strong",
-    artist: "One Direction",
-    img: "music5",
-    src: "music5"
-  },
-  {
-    name: "Tek It",
-    artist: "Cafuné",
-    img: "music6",
-    src: "music6"
-  },
-
-
 
 
 
